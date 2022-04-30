4199fe4baf77c7e980776429980e2dd4cb370e02
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-04-30 13:00:56 +0200

    get all players and teams

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,50 @@
-async function getPlayerByTeam() {
-	var response = await fetch("https://www.balldontlie.io/api/v1/teams", {
+function getAllTeams() {
+	var teams = await fetch("https://www.balldontlie.io/api/v1/teams", {
 		method: "GET"
 	});
 
-	var jsonObj = await response.json();
-	console.log(jsonObj);	
+	var jsonT = await teams.json();
+	console.log(jsonT);
 }
 
+async function getAllPlayers() {
+	var player = {
+		id_team: "",
+		team: "",
+		abbreviation: "",
+		id: "",
+		firstname: "",
+		lastname: "",
+		role: ""
+	}
 
-getPlayerByTeam();
+	var players = [];
+
+	let i = 1;
+
+	do {
+		var object = await fetch("https://www.balldontlie.io/api/v1/players?page="+i+"&per_page=100", {
+			method: "GET"
+		});
+		var jsonP = await object.json();
+		console.log(jsonP);
+
+		for (let j = 0; j <= Object.keys(jsonP.data).at(-1); j++) {
+			player.id_team = jsonP.data[j].team.id;
+			player.team = jsonP.data[j].full_name;
+			player.abbreviation = jsonP.data[j].abbreviation;
+			player.id = jsonP.data[j].id;
+			player.firstname = jsonP.data[j].first_name;
+			player.lastname = jsonP.data[j].last_name;
+			player.role = jsonP.data[j].position;
+
+			players.push(player);
+		}
+		i++;
+	} while(jsonP.meta.next_page != null);
+	console.log(players);
+}
+
+getAllTeams();
+getAllPlayers();
 
