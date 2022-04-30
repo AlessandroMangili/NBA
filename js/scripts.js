ca6387c93d6db65c115df32e3b9a715f81f3c147
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-05-01 00:48:32 +0200

    Added all teams logo & created the object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,64 @@
+/*var set = {
+	[key] : {
+		team: "ciao",
+		abbreviation: "",
+		players : [{
+			id : "",
+			firstname : "",
+			lastname : "",
+			role : "",
+		}]
+	}
+}*/
+var set = {};
+
+/**{ 
+ * 		1 : 
+ * 		{
+ * 			team : "Lakers",
+ * 			abbrevation : "LA",
+ * 			players : 
+ * 			[
+ * 				[ 
+ * 					id : "10",
+ * 					firstname : "James",
+ * 					lastname : "Lebron",
+ * 					role : "R",
+ * 				]
+ * 				[
+ *	 				id : "10",
+ * 					firstname : "James",
+ * 					lastname : "Lebron",
+ * 					role : "R",
+ * 				]
+ * 			]
+ * 		}
+ * 		2 :
+ * 		{ 
+ * 			team : "Lakers",
+ * 			abbrevation : "LA",
+ * 			players : 
+ * 			[
+ * 				[ 
+ * 					id : "10",
+ * 					firstname : "James",
+ * 					lastname : "Lebron",
+ * 					role : "R",
+ * 				]
+ * 				[
+ *	 				id : "10",
+ * 					firstname : "James",
+ * 					lastname : "Lebron",
+ * 					role : "R",
+ * 				]
+ * 			]
+ * 		}
+ * }
+ * 
+ * 
+ * 
+ **/
+
 async function getAllTeams() {
 	var teams = await fetch("https://www.balldontlie.io/api/v1/teams", {
 		method: "GET"
@@ -5,14 +66,13 @@ async function getAllTeams() {
 
 	var jsonT = await teams.json();
 
+
 	for (let i = 0; i <= Object.keys(jsonT.data).at(-1); i++) {
-		document.getElementById('teams').innerHTML += "<div id='icon'><img width='75px' src='./Loghi/"+ jsonT.data[i].name +".png' alt='"+ jsonT.data[i].name +".png'></img>"+ jsonT.data[i].full_name + "</div>";
+		document.getElementById('teams').innerHTML += "<div id='icon'><img width='75px' src='./Loghi/"+ jsonT.data[i].name +".png' alt='"+ jsonT.data[i].name +".png'></img></div> <p>"+ jsonT.data[i].full_name + "</p>";
 	}
 }
 
 async function getAllPlayers() {
-	var players = [];
-
 	let i = 1;
 
 	do {
@@ -22,20 +82,29 @@ async function getAllPlayers() {
 		var jsonP = await object.json();
 
 		for (let j = 0; j <= Object.keys(jsonP.data).at(-1); j++) {
-			let player = new Object();
-			player.id_team = jsonP.data[j].team.id;
-			player.team = jsonP.data[j].team.full_name;
-			player.abbreviation = jsonP.data[j].team.abbreviation;
-			player.id = jsonP.data[j].id;
-			player.firstname = jsonP.data[j].first_name;
-			player.lastname = jsonP.data[j].last_name;
-			player.role = jsonP.data[j].position;
-
-			players.push(player);
+			if(set[jsonP.data[j].team.id] != undefined) {
+				set[jsonP.data[j].team.id].players.push({
+					id : jsonP.data[j].id,
+					firstname : jsonP.data[j].first_name,
+					lastname : jsonP.data[j].last_name,
+					role : jsonP.data[j].position
+				})				
+			} else {
+				set[jsonP.data[j].team.id] = {
+					team: jsonP.data[j].team.full_name,
+					abbreviation : jsonP.data[j].team.abbreviation,
+					players : [{
+						id : jsonP.data[j].id,
+						firstname : jsonP.data[j].first_name,
+						lastname : jsonP.data[j].last_name,
+						role : jsonP.data[j].position
+					}]
+				}
+			}
 		}
 		i++;
 	} while(jsonP.meta.next_page != null);
-	console.log(players);
+	console.log(set);
 }
 
 getAllPlayers();
