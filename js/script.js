753c04243ac6751f3b6754be5501e3a24f892459
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-05-06 12:15:09 +0200

    adding link function for show players of team clicked

--- js/script.js
+++ js/script.js
@@ -29,11 +29,16 @@ async function getAllTeams() {
 		method: "GET"
 	});
 
-	var jsonT = await teams.json();
-	for (let i = 0; i <= Object.keys(jsonT.data).at(-1); i++) {
-		//$("#owl-carousel").append('');
+	var json = await teams.json();
+	for (let i = 0; i <= Object.keys(json.data).at(-1); i++) {
 		$("#owl-carousel").append('<div class="item">New slide</div>');
-		$owl.trigger('add.owl.carousel', ['<div class="card"> <div class="ms-2 me-2"><a href="#!"><img src="./Loghi/'+ jsonT.data[i].name +'.png" class="card-img-top"></a><div class="card-body">'+ jsonT.data[i].full_name +'</div> </div> </div>']).trigger("refresh.owl.carousel");	
+		$owl.trigger('add.owl.carousel', ['<div class="card"> <div class="ms-2 me-2"><a value="'+ json.data[i].name +'" onclick="getPlayersTeam(this.getAttribute('+"'value'"+'))" href="#!"><img src="./Loghi/'+ json.data[i].name +'.png" class="card-img-top"></a><div class="card-body">'+ json.data[i].full_name +'</div> </div> </div>']).trigger("refresh.owl.carousel");	
 	}
 }
+
+function getPlayersTeam(team_name) {
+	alert(team_name);
+}
+
+
 
