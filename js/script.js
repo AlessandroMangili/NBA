205ed46330e54ca8c5244fe0b6ed0ed2c58a2576
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-05-06 20:17:35 +0200

    use of localStorage and adding season input stats

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var $owl = $(".owl-carousel").owlCarousel({
 	autoplay: true,
 	autoplayhoverpause: true,
 	autoplaytime: 100,
-	items: 4,
 	loop: true,
 	responsive: {
 		0: {
@@ -25,13 +24,18 @@ var $owl = $(".owl-carousel").owlCarousel({
 });
 
 async function getAllTeams() {
-	var teams = await fetch("https://www.balldontlie.io/api/v1/teams", {
-		method: "GET"
-	});
-
-	var json = await teams.json();
+	if(localStorage.getItem("teams") == null){
+		//alert("Take data from API");
+		var object = await fetch("https://www.balldontlie.io/api/v1/teams", {
+			method: "GET"
+		});
+		var json = await object.json();
+		localStorage.setItem("teams",JSON.stringify(json));
+	} else {
+		var json = JSON.parse(localStorage.getItem("teams"));
+	}
+	
 	for (let i = 0; i <= Object.keys(json.data).at(-1); i++) {
-		$("#owl-carousel").append('<div class="item">New slide</div>');
 		$owl.trigger('add.owl.carousel', ['<div class="card"> <div class="ms-2 me-2"><a value="'+ json.data[i].name +'" onclick="getPlayersTeam(this.getAttribute('+"'value'"+'))" href="#!"><img src="./Loghi/'+ json.data[i].name +'.png" class="card-img-top"></a><div class="card-body">'+ json.data[i].full_name +'</div> </div> </div>']).trigger("refresh.owl.carousel");	
 	}
 }
--- js/statScript.js
+++ js/statScript.js
@@ -1,5 +1,4 @@
 /**
-
 var set = {};
 
 async function getAllPlayersByTeam() {
@@ -38,6 +37,18 @@ async function getAllPlayersByTeam() {
 }
 **/
 
+var input_player = document.getElementById("playerName");
+var input_season = document.getElementById("season");
+var btn = document.getElementById("searchPlayer");
+var score = document.getElementById("score");
+
+btn.addEventListener('click', (e) => {
+	e.preventDefault();
+	stats(input_season.value, input_player.value);
+});
+
+
+
 // Funzione che dato il nome di un giocatore, ritorna il suo id se questo è presente
 async function playerIdByName(playerName) {
 	var playerObj = await fetch("https://www.balldontlie.io/api/v1/players?search="+playerName, {
@@ -48,47 +59,59 @@ async function playerIdByName(playerName) {
 	return player.data;
 }
 
-// Funzione che data la stagione e l'id di un giocatore, ritorna le statistiche per quella stagione se il giocatore esiste e se ha giocato in quella stagione
+// Funzione che data la stagione, non nulla o vuota, e l'id di un giocatore, ritorna le statistiche per quella stagione se
+// il giocatore esiste e se ha giocato in quella stagione
 async function individualStats(season, player_id) {
-	if (season != null) {
-		var statsObj = await fetch("https://www.balldontlie.io/api/v1/season_averages?season="+season+"&player_ids[]="+player_id, {
-			method: "GET"
-		});
-	} else {
-		var statsObj = await fetch("https://www.balldontlie.io/api/v1/season_averages?player_ids[]="+player_id, {
-			method: "GET"
-		});	
-	}
+	var statsObj = await fetch("https://www.balldontlie.io/api/v1/season_averages?season="+season+"&player_ids[]="+player_id, {
+		method: "GET"
+	});
+
 	var stats = await statsObj.json();
 	return stats.data;
-
 }
 
-//funizione sincrona che data la stagione e il nome del giocatore, richiama a sua volta prima la funzione asincrona per ottenere l'id tramite il nome del giocatore, se questo esiste, e una volta ottnuto, verrà richiamata la funzione asincrona per ottenere le statistiche della stagione passata come parametro, se null allora sarà la stagione corrente, per il determinato giocatore, sempre che quel giocatore abbia giocato in quella stagione.
+/*
+funizione sincrona che data la stagione e il nome del giocatore, richiama a sua volta prima la funzione asincrona
+per ottenere l'id tramite il nome del giocatore, se questo esiste, e una volta ottnuto, verrà richiamata la funzione asincrona per ottenere 
+le statistiche della stagione passata come parametro, se null allora sarà la stagione corrente, per il determinato giocatore, sempre che quel 
+giocatore abbia giocato in quella stagione.
+*/
 function stats(season, playerName) {
+	season = season === "" ? new Date().getFullYear() - 1: season;
+
 	playerIdByName(playerName)
 	.then(data => 
 		{
-			console.log(data);
 			if (data.length > 1) {
 				alert("Presenza di più " + playerName + "! inserisci il nome completo");
 			} else if (data.length < 1) {
 				alert("Non ci sono giocatori con il seguente nome: " + playerName);
 			} else {
-				individualStats(season, data[0].id)
-				.then(stats => 
-					{
-						season = (season == null) ? "2021" : season;
-						if (stats.length > 1) {
-							alert("Inserisci nome e conogme");
-						} else if (stats.length < 1) {
-							alert("Non ci sono statitiche riguandanti il giocatore: "+ data[0].first_name + " " + data[0].last_name +" nella stagione " + season);
-						} else {
-							console.log(stats)
-							document.getElementById('score').innerHTML = "<p>Nome : " + data[0].first_name + " " + data[0].last_name +"</p><p>Squadra : " + data[0].team.full_name +"</p><p>Stagione: "+ season +" </p><p>Media punti : " + stats[0].pts +"</p><p> Media minuti giocati : " + stats[0].min;
+				if(localStorage.getItem(season + " " + data[0].id) == null) {
+					individualStats(season, data[0].id)
+					.then(stats => 
+						{
+							localStorage.setItem(season + " " + data[0].id, JSON.stringify(stats));
+							if (stats.length > 1) {
+								alert("Inserisci nome e conogme");
+							} else if (stats.length < 1) {
+								alert("Non ci sono statitiche riguandanti il giocatore: "+ data[0].first_name + " " + data[0].last_name +" nella stagione " + season);
+							} else {
+								//console.log(stats);
+								document.getElementById('score').innerHTML = "<p>Nome : " + data[0].first_name + " " + data[0].last_name +"</p><p>Stagione: "+ season +" </p><p>Media punti : " + stats[0].pts +"</p><p> Media minuti giocati : " + stats[0].min;
+							}
 						}
+					)
+				} else {
+					var stats = JSON.parse(localStorage.getItem(season + " " + data[0].id));
+					if (stats.length > 1) {
+						alert("Inserisci nome e conogme");
+					} else if (stats.length < 1) {
+						alert("Non ci sono statitiche riguandanti il giocatore: "+ data[0].first_name + " " + data[0].last_name +" nella stagione " + season);
+					} else {
+						document.getElementById('score').innerHTML = "<p>Nome : " + data[0].first_name + " " + data[0].last_name +"</p><p>Stagione: "+ season +" </p><p>Media punti : " + stats[0].pts +"</p><p> Media minuti giocati : " + stats[0].min;
 					}
-				)
+				}
 			}
 		}
 	);
