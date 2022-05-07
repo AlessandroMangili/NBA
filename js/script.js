2cf5efe60a7126d209f1612b85edad0e1a52d642
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-05-07 16:47:16 +0200

    adding file players split for teams

--- js/script.js
+++ js/script.js
@@ -34,7 +34,6 @@ async function getAllTeams() {
 	} else {
 		var json = JSON.parse(localStorage.getItem("teams"));
 	}
-	
 	for (let i = 0; i <= Object.keys(json.data).at(-1); i++) {
 		$owl.trigger('add.owl.carousel', ['<div class="card"> <div class="ms-2 me-2"><a value="'+ json.data[i].name +'" onclick="getPlayersTeam(this.getAttribute('+"'value'"+'))" href="#!"><img src="./Loghi/'+ json.data[i].name +'.png" class="card-img-top"></a><div class="card-body">'+ json.data[i].full_name +'</div> </div> </div>']).trigger("refresh.owl.carousel");	
 	}
