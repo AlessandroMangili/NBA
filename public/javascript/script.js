c072c28006e1ab4dc5069b0880844fa54d195500
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-05-24 18:17:12 +0200

    adding card for player info

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -64,37 +64,63 @@ function getPlayersTeam(team_id) {
 	  			for(let i = 0; i < players.length; i++) {
 	  				let player = players[i].split(';');
 	  				if (player[0] == team_id) {
+						
 						let div1 = document.createElement("div");
 						div1.setAttribute("class", "col-sm-4 pb-2");
-						let div2 = document.createElement("div");
-						div2.setAttribute("class", "card border-secondary text-center");
-						let div3 = document.createElement("div");
-						div3.setAttribute("class", "card-header");
-						let header = document.createElement("h5");
-						header.innerText = player[2];
-						div3.appendChild(header);
-						let div4 = document.createElement("div");
-						div4.setAttribute("class", "card-body");
+						let container = document.createElement("div");
+						container.setAttribute("class", "card-container");
+
+						let front = document.createElement("div");
+						front.setAttribute("class", "card border-secondary card-front text-center");
+						let header = document.createElement("div");
+						header.setAttribute("class", "card-header");
+						let maglia = document.createElement("h5");
+						maglia.innerText = player[2];
+						header.appendChild(maglia);
+						let body = document.createElement("div");
+						body.setAttribute("class", "card-body");
 						let title = document.createElement("h5");
 						title.setAttribute("class", "card-title");
 						title.innerText =  player[1];
-						div4.appendChild(title);
+						body.appendChild(title);
 						let ruolo = document.createElement("p");
 						ruolo.setAttribute("class", "card-text");
 						ruolo.innerText =  player[3];
-						div4.appendChild(ruolo);
+						body.appendChild(ruolo);
 						let nascita = document.createElement("p");
 						nascita.setAttribute("class", "card-text");
 						nascita.innerText =  player[6];
-						div4.appendChild(nascita);
+						body.appendChild(nascita);
 						let a = document.createElement("a");
 						a.setAttribute("class", "btn btn-primary");
 						a.setAttribute("href", "#!");
 						a.innerText = "Full bio";
-						div4.appendChild(a);
-						div2.appendChild(div3);
-						div2.appendChild(div4);
-						div1.appendChild(div2);
+						body.appendChild(a);
+
+						front.appendChild(header);
+						front.appendChild(body);
+
+						let back = document.createElement("div");
+						back.setAttribute("class", "card border-secondary card-back text-center");
+						let header_back = document.createElement("div");
+						header_back.setAttribute("class", "card-header");
+						let maglia_back = document.createElement("h5");
+						maglia_back.innerText = player[2];
+						header_back.appendChild(maglia_back); 
+						let body_back = document.createElement("div");
+						body_back.setAttribute("class", "card-body");
+						let a_back = document.createElement("a");
+						a_back.setAttribute("class", "btn btn-primary");
+						a_back.setAttribute("href", "#!"); 
+						a_back.innerText = "Back";
+						body_back.appendChild(a_back);
+
+						back.appendChild(header_back);
+						back.appendChild(body_back);
+
+						container.appendChild(front);
+						container.appendChild(back);
+						div1.appendChild(container);
 						player_div.appendChild(div1);
 	  				}
 	  			}
