2aaf73c70bf917b8ad641d5a5d7a596abd74631b
A-mangio <alessandro.mangili1@studenti.unimi.it>
2022-05-27 12:00:32 +0200

    adding routes to node server

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const res = require('express/lib/response');
+const path = require('path');
 const app = express();
 const host = "127.0.0.1"
 const port = process.env.PORT || 8000;
 app.set('port', port);
 
-
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    console.log(__dirname);
-    res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get('/statistics/table', (req, res) => {
+    //console.log("Statistiche table", __dirname);
+    res.sendFile(path.join(__dirname, "/public/pages/IndividualStatistics-table.html"));
+})
+
+app.get('/statistics/chart', (req, res) => {
+    //console.log("Statistiche chart", __dirname);
+    res.sendFile(path.join(__dirname + "/public/pages/IndividualStatistics-chart.html"));
+})
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/pages/404.html"));
 });
 
 
