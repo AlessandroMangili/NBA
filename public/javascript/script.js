6e395176bc24758ff3f987bce7362dc0540f631b
Serigne-Mbaye <serignedarou.mbaye@studenti.unimi.it>
2022-06-23 02:57:18 +0200

    fixing Dark mode & footer

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -180,25 +180,3 @@ function httpGet(url, callback) {
 	request.send();
 }
 
-/*
- * Dark mode
- */
-var darkSwitch = document.getElementById("darkSwitch");
-window.addEventListener("load", (function () {
-    if (darkSwitch) {
-        initTheme();
-        darkSwitch.addEventListener("change", (function () {
-            resetTheme()
-        }))
-    }
-}));
-
-function initTheme() {
-    var darkThemeSelected = localStorage.getItem("darkSwitch") !== null && localStorage.getItem("darkSwitch") === "dark";
-    darkSwitch.checked = darkThemeSelected; darkThemeSelected ? document.body.setAttribute("data-theme", "dark") : document.body.removeAttribute("data-theme")
-} function resetTheme() {
-    if (darkSwitch.checked) { document.body.setAttribute("data-theme", "dark"); localStorage.setItem("darkSwitch", "dark") } else {
-        document.body.removeAttribute("data-theme");
-        localStorage.removeItem("darkSwitch")
-    }
-}
